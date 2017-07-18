72f08f53f1c9e1f09f74cbdce29a93851a43ac83
zhangbin <superbinlin@163.com>
2017-07-18 11:43:57 +0800

    add share function

--- resource/views/album.js
+++ resource/views/album.js
@@ -64,6 +64,23 @@ class Album extends React.Component{
           wx.error(function(res){
               console.log('error:'+JSON.stringify(res));
           });
+
+          wx.onMenuShareAppMessage({
+            title: 'Binlin相册', // 分享标题
+            desc: '嘿嘿嘿', // 分享描述
+            link: 'http://binlin.site/#/album', // 分享链接
+            imgUrl: 'http://www.warmwood.com/images/s1.jpg', // 分享图标
+            type: '', // 分享类型,music、video或link，不填默认为link
+            dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert("分享");
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+                alert("取消分享");
+            }
+          });
         })
     }).catch((err)=>{
       callback({
