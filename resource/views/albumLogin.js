eacaddf00c9a486bafe0eecea8592815fa127780
zhangbin <superbinlin@163.com>
2017-07-22 23:05:02 +0800

    add register page

--- resource/views/albumLogin.js
+++ resource/views/albumLogin.js
@@ -14,16 +14,44 @@ class Login extends React.Component{
   constructor(props) {
     super(props);
   
-    this.state = {};
+    this.state = {
+      isLoginAction:true
+    };
+  }
+
+  /**
+   * 前往注册页
+   * @return {[type]} [description]
+   */
+  register(){
+    this.setState({isLoginAction:false})
+  }
+
+  /**
+   * 返回登录页
+   * @return {[type]} [description]
+   */
+  backLogin(){
+    this.setState({isLoginAction:true})
   }
 
   render(){
+    let {isLoginAction} = this.state;
+    let login = classNames({
+      "login-form":true,
+      "active":isLoginAction
+    })
+    let register = classNames({
+      "register-form":true,
+      "initpos":true,
+      "active":!isLoginAction
+    })
     return (
       <DocumentTitle title='Login'>
         <div>
           <div className="login-wp">
             <h3 className="login-title">Binlin Album</h3>
-            <ul className="login-form">
+            <ul className={login}>
               <li>
                 <i className="icon-fz ion-ios-person-outline"></i>
                 <div className="login-input-wp">
@@ -43,8 +71,42 @@ class Login extends React.Component{
               <li className="signin noline">
                 <a className="signin-btn">Sign In</a>
               </li>
-              <li className="noline register">
+              <li className="noline register" onClick={()=>{this.register()}}>
                 <a>Don't have an account? <span>Sign Up</span></a>
+              </li>
+            </ul>
+            <ul className={register}>
+              <li>
+                <i className="icon-fz ion-ios-person-outline"></i>
+                <div className="login-input-wp">
+                  <input type="text" className="login-input" placeholder="Full name"/>
+                </div>
+              </li>
+              <li>
+                <i className="icon-fz ion-ios-locked-outline"></i>
+                <div className="login-input-wp">
+                  <input type="text" className="login-input" placeholder="Your password"/>
+                </div>
+              </li>
+              <li>
+                <i className="icon-fz ion-ios-locked-outline"></i>
+                <div className="login-input-wp">
+                  <input type="text" className="login-input" placeholder="Confire your password"/>
+                </div>
+              </li>
+              <li>
+                <i className="icon-fz ion-iphone"></i>
+                <div className="login-input-wp">
+                  <input type="text" className="login-input phone-input" placeholder="Phone"/>
+                  <a className="confire-input">获取验证码</a>
+                </div>
+              </li>
+              <li className="signin noline">
+                <a className="signin-btn">Sign Up</a>
+              </li>
+              <li className="noline">
+                <p className="half-wp col-left"></p>
+                <p className="half-wp col-right fpw" onClick={()=>{this.backLogin()}}>已有账号？返回登录页</p>
               </li>
             </ul>
           </div>
