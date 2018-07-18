aad7625c60f582e91a2114e3a5a253d066a267ed
zhangbin <superbinlin@163.com>
2018-07-18 17:12:33 +0800

    change get photo by api to storage

--- resource/views/photoshow.js
+++ resource/views/photoshow.js
@@ -143,7 +143,7 @@ class Photoshow extends React.Component{
 
   render(){
     let { masonryOptions, photoSwipe, isphotoSwipeOpen, currentCity} = this.state;
-    let childElements = photoSwipe.item.map((element, index) => {
+    let childElements = photoSwipe.items.map((element, index) => {
      return (
         <div className="image-element-class col-lg-3 col-md-4 col-sm-6 col-xs-12" key={index} onClick={()=>{this.showPhotoswipe(index)}}>
           <div className="img-wp">
